88e709b5ac7a1ff775ef418a2a7fa422ceeb340f
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-02-27 16:03:44 +0100

    GraphQL Subscriptions Setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+import express from 'express'
+import { ApolloServer, graphqlExpress } from 'apollo-server-express'
 import models from './models/index'
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors'
 import addUser from './middleware/addUser'
+import http from 'http'
+
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+import { execute, subscribe } from 'graphql';
+import { PubSub } from 'graphql-subscriptions';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 
 const SECRET = "a string that you would never be able to guess"
 const SECRET2 = "another string, just used for refreshing"
@@ -22,8 +29,7 @@ app.use(cors('*'))
 //add User Id to the request
 app.use(addUser)
 
-const server =  () => {
-  return new ApolloServer({ typeDefs, resolvers,  playground: true, 
+const server =  new ApolloServer({ typeDefs, resolvers,  playground: true, 
     context: ({ req }) => {
 
       // retrieve user from the request item, added in addUser
@@ -38,11 +44,17 @@ const server =  () => {
       };
     }
   });
-} 
 
-server().applyMiddleware({ app });
+server.applyMiddleware({ app })
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
 
 // { force: true } inside sync to drop db
 models.sequelize.sync().then(function () {
-  app.listen(PORT);
+  httpServer.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
+  })  
 });
