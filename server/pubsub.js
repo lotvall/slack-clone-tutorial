3955aaec15f65afeb6aa9d333aa9f9f31aa7ca6e
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-03-20 23:07:39 +0100

    added redis to subscriptions

--- server/pubsub.js
+++ server/pubsub.js
@@ -1,3 +1,12 @@
-import { PubSub } from "apollo-server";
+import { RedisPubSub } from 'graphql-redis-subscriptions'
 
-export default new PubSub()
+export default new RedisPubSub({
+  connection: {
+    host: '127.0.0.1',
+    port: 6379,
+    retry_strategy: options => {
+      // reconnect after upto 3000 milis
+      return Math.max(options.attempt * 100, 3000);
+    }
+  }
+});
